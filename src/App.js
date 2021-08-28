6ac9201de243b98578138a556c1221ed912aa5e6
Roberto Abreu <abreuroberto2015@gmail.com>
2021-08-28 17:25:44 -0300

    28/08

--- src/App.js
+++ src/App.js
@@ -2,36 +2,15 @@ import React,{useState} from 'react'
 
 export default function App(){
 
-  const [cor,setCor]=useState(1)
-
-  const vermelho ={color:'red'}
-  const verde ={color:'green'} 
-  const azul ={color:'blue'}
-
-  const retCor=(c)=>{
-    if(c==1){
-      return vermelho
-    }else if(c==2){
-      return verde
-    }else{
-      return azul
-    }
-  }
-
-  const mudaCor=()=>{
-    setCor(cor+1)
-    if(cor > 2){
-      setCor(1)
-    }
-  }
-
-  setInterval(mudaCor,1000)
-  
+  const [nome,setNome]=useState('')
+    
   return (
     <>
-      <h1 style={retCor(cor)}>CFB Cursos</h1>
-      <button onClick={()=>mudaCor()}>Muda cor</button>
-    </>
+      <label>Digite seu Nome</label>
+      <input type="text" name="fnome" value={nome} 
+      onChange={(e)=>setNome(e.target.value)}/>
+      <p>Nome digitado: {nome}</p>        
+    </>  
   );
 }
 
