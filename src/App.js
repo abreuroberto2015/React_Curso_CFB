5a5cb63733e14e2b26ba446455f7111c8fc3f126
Roberto Abreu <abreuroberto2015@gmail.com>
2021-09-07 14:48:43 -0300

    07/09/21

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Carro from './componentes/Carro'
 
 
 export default function App() {  
+  const [carro,setCarro] =useState(true)
+  const mostrarOcultarCarro=()=>{
+    setCarro(!Carro)
+  }
   return (
     <>
     <h1>Componentes de Classe</h1>
-    <Carro fator={1}/>
-      
+    {carro ? <carro fator={10}/> : ''}
+    <button onClick={() => mostrarOcultarCarro()}>Ocultar</button>      
     </>
   );
 }
--- src/componentes/Carro.js
+++ src/componentes/Carro.js
@@ -1,40 +1,50 @@
 import React from 'react'
 
-export default  class Carro extends React.Component {
-    constructor(props){
+export default class Carro extends React.Component {
+    constructor(props) {
         super(props)
-        this.modelo='Golf'
-        this.state ={
-            ligado:false,
-            velAtual:0,
+        this.modelo = 'Golf'
+        this.state = {
+            ligado: false,
+            velAtual: 0,
         }
         this.ld = this.ligarDesligar.bind(this)
     }
     ligarDesligar() {
-      //this.state.ligado = true
-      this.setState({ligado:!this.state.ligado})
-      //this.setState(
-      //    (state) => ({
-      //        ligado:!state.ligado
-      //  })
-      //)
+        //this.state.ligado = true
+        this.setState({ ligado: !this.state.ligado })
+        //this.setState(
+        //    (state) => ({
+        //        ligado:!state.ligado
+        //  })
+        //)
     }
     acelerar() {
         this.setState(
-            (state, props)=>(
-               {velAtual:state.velAtual + props.fator}
-               ) 
-        )          
+            (state, props) => (
+                { velAtual: state.velAtual + props.fator }
+            )
+        )
     }
+    componentDidMount() {
+        console.log('O carro foi criado')
+    }
+    componentDidUpdate() {
+        console.log('O carro foi atualizado')
+    }
+    componentWillUnmount() {
+        console.log('O carro foi removido')
+    }
+
     render() {
         return (
             <div>
-            <h1>Meu Carro</h1>
-            <p>Modelo: {this.modelo}</p>  
-            <p>ligado: {this.state.ligado ? 'sim' : 'Não'}</p>   
-            <p>velAtual: {this.state.velAtual}</p>  
-            <button onClick={this.ld}>{this.state.ligado ? 'Desligar Carro' : 'Ligar Carro'}</button>       
-            <button onClick={()=>this.acelerar()}>Acelerar</button>         
+                <h1>Meu Carro</h1>
+                <p>Modelo: {this.modelo}</p>
+                <p>ligado: {this.state.ligado ? 'sim' : 'Não'}</p>
+                <p>velAtual: {this.state.velAtual}</p>
+                <button onClick={this.ld}>{this.state.ligado ? 'Desligar Carro' : 'Ligar Carro'}</button>
+                <button onClick={() => this.acelerar()}>Acelerar</button>                
             </div>
         )
     }
