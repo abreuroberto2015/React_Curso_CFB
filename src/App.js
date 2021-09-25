058d8b29a065770d80af3670cac4dc2a3e96c54d
Roberto Abreu e Silva <abreuroberto2015@gmail.com>
2021-09-25 19:29:47 -0300

    25/9

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React, { useState } from 'react'
 
-import Carro from './componentes/Carro'
+const carros = [
+  { categoria: "Esporte", preco: "110000", modelo: "Golf GTI" },
+  { categoria: "Esporte", preco: "120000", modelo: "Camaro" },
+  { categoria: "SUV", preco: "85000", modelo: "HRV" },
+  { categoria: "SUV", preco: "83000", modelo: "T-Cross" },
+  { categoria: "Utilitario", preco: "125000", modelo: "Hilux" },
+  { categoria: "Utilitario", preco: "900000", modelo: "Ranger" }
+]
 
+const linhas = (cat) => {
+  const li = []
+  carros.forEach(
+    (carro) => {
+      if (carro.categoria.toUpperCase() == cat.toUpperCase() || cat.toUpperCase() == '') {
+        li.push(
+          <tr>
+            <td>{carro.categoria}</td>
+            <td>{carro.preco}</td>
+            <td>{carro.modelo}</td>
+          </tr>
+        )
+      }
+    }
+  )
+  return li
+}
 
-export default function App() {  
-  const [carro,setCarro] =useState(true)
-  const mostrarOcultarCarro=()=>{
-    setCarro(!Carro)
-  }
+const TabelaCarros = (cat) => {
+  return (
+    <table border='1' style={{ borderCollapse: 'collapse' }}>
+      <thead>
+        <tr>
+          <th>Categoria</th><th>Preço</th><th>Modelo</th>
+        </tr>
+      </thead>
+      <tbody>
+        {linhas(cat)}
+      </tbody>
+    </table>
+  )
+}
+
+const pesquisaCategoria = (cat, scat)=>{
+  return (
+    <div>
+      <label>Digite uma categoria</label>
+      <input type="text" value={cat} onChange={(e) => scat(e.target.value)} />
+    </div>
+  )
+
+}
+export default function App() {
+  const [categoria, setCategoria] = useState('')
   return (
     <>
-    <h1>Componentes de Classe</h1>
-    {carro ? <carro fator={10}/> : ''}
-    <button onClick={() => mostrarOcultarCarro()}>Ocultar</button>      
+      {pesquisaCategoria(categoria, setCategoria)}
+      <br />
+      {TabelaCarros(categoria)}
     </>
   );
 }
-
